7ad358dc5d19f48172afc49fb86101223870327c
ahmadullo2023 <ahmadullo2023@gmail.com>
2024-03-19 16:26:26 +0500

    some change

--- lib/src/widget/app.dart
+++ lib/src/widget/app.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:hot_shop/src/widget/splash_page.dart';
 import 'package:provider/provider.dart';
 import '../controller/provider.dart';
+import '../screens/onboarding.dart';
 
 class App extends StatefulWidget {
   const App({Key? key}) : super(key: key);
@@ -17,7 +18,8 @@ class _AppState extends State<App> {
       create: (context) => ProFunc(),
       child: const MaterialApp(
         debugShowCheckedModeBanner: false,
-        home: SplashPage(),
+      //  home: MyApp(),
+        home:  SplashPage(),
       ),
     );
   }
